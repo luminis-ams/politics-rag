1a4162e9f2f0f5f2f49b63882a24ca5e5f551a5b
Jettro Coenradie <jettro@coenradie.com>
2023-11-17 14:30:58 +0100

    Initial commit

---
+++ src/main/java/eu/luminis/politicsrag/PoliticsRagApplication.java
@@ -0,0 +1,13 @@
+package eu.luminis.politicsrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticsRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoliticsRagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/luminis/politicsrag/PoliticsRagApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.luminis.politicsrag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliticsRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
